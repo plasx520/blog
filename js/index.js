db46907f0289bc6f4ea0e1a32472f41a48a974b4
怕冷爱上雪 <1400000222@qq.com>
2023-01-16 13:50:11 +0800

    随机

--- js/index.js
+++ js/index.js
@@ -16,3 +16,17 @@
     var x = $(".down-list").width() * index + parseInt($(".down-list").css("margin-right")) * index;
     $(".down-list .tpl-tr").css("transform", "translateX(-" + x + "px)");
 });
+
+//首页大卡片恢复显示
+$(".div-card3").hover(function () {
+}, function () {
+  hoverOnCommentBarrage = false;
+  document.getElementById("todayCard").classList.remove('hide');
+  document.getElementById('todayCard').style.zIndex = 1;
+});
+
+  function buttonClick() {
+    if (document.getElementById("todayCard")) {
+      document.getElementById("todayCard").classList.add('hide');
+    }
+  }
