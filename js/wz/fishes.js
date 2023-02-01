6fb9ec77784cd3034535645197d89493b31e21b6
怕冷爱上雪 <1400000222@qq.com>
2023-02-01 18:02:58 +0800

    更新

--- js/wz/fishes.js
+++ js/wz/fishes.js
@@ -6,7 +6,7 @@ function fish() {
     ),
     $(".container").css({
       width: "100%",
-      height: "160px",
+      height: "170px",
       margin: 0,
       padding: 0,
     }),
@@ -15,7 +15,7 @@ function fish() {
       "text-align": "center",
       top: 0,
       right: 0,
-      left: 0,
+      left: -3,
       bottom: 0,
     }),
     this
