3ce0e5519b4045e8f60c16c59bc85c03fca48ecd
怕冷爱上雪 <1400000222@qq.com>
2023-03-03 22:09:36 +0800

    1

--- js/index.js
+++ js/index.js
@@ -1,32 +1,40 @@
+/*pjax加载函数*/
+whenDOMReady()
+function whenDOMReady() {
+  if (typeof pjaxqj === 'function') {pjaxqj();}
+}
+document.addEventListener("pjax:complete", whenDOMReady) // pjax加载完成（切换页面）后再执行一次
+document.addEventListener("DOMContentLoaded",whenDOMReady);//第一次
+
 /*首页下载板块更新悬浮效果*/
 $(".down-index .type a").hover(function (e) {
-  $(".down-index .type .hover").removeClass("hover");
-  $(this).addClass("hover");
-  /*计算当前应该偏移的高度*/
-  var index = $(".down-index .type .hover").index();
-  var x = $(".down-index").width() * index + parseInt($(".down-index").css("margin-right")) * index;
-  $(".down-index .tpl-tr1").css("transform", "translateX(-" + x + "px)");
+$(".down-index .type .hover").removeClass("hover");
+$(this).addClass("hover");
+/*计算当前应该偏移的高度*/
+var index = $(".down-index .type .hover").index();
+var x = $(".down-index").width() * index + parseInt($(".down-index").css("margin-right")) * index;
+$(".down-index .tpl-tr1").css("transform", "translateX(-" + x + "px)");
 });
 /*首页下载板块更新悬浮效果*/
 $(".down-list .type a").hover(function (e) {
-  $(".down-list .type .hover").removeClass("hover");
-  $(this).addClass("hover");
-  /*计算当前应该偏移的高度*/
-  var index = $(".down-list .type .hover").index();
-  var x = $(".down-list").width() * index + parseInt($(".down-list").css("margin-right")) * index;
-  $(".down-list .tpl-tr").css("transform", "translateX(-" + x + "px)");
+$(".down-list .type .hover").removeClass("hover");
+$(this).addClass("hover");
+/*计算当前应该偏移的高度*/
+var index = $(".down-list .type .hover").index();
+var x = $(".down-list").width() * index + parseInt($(".down-list").css("margin-right")) * index;
+$(".down-list .tpl-tr").css("transform", "translateX(-" + x + "px)");
 });
 
 //首页大卡片恢复显示
 $(".div-card3").hover(function () {
 }, function () {
-  hoverOnCommentBarrage = false;
-  document.getElementById("todayCard").classList.remove('hide');
-  document.getElementById('todayCard').style.zIndex = 1;
+hoverOnCommentBarrage = false;
+document.getElementById("todayCard").classList.remove('hide');
+document.getElementById('todayCard').style.zIndex = 1;
 });
 
 function buttonClick() {
-  if (document.getElementById("todayCard")) {
-    document.getElementById("todayCard").classList.add('hide');
-  }
+if (document.getElementById("todayCard")) {
+  document.getElementById("todayCard").classList.add('hide');
+}
 }
