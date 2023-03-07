ec75f0e82e045dea514ee077add4f06dcb9a7861
怕冷爱上雪 <1400000222@qq.com>
2023-03-07 23:37:21 +0800

    更新

--- js/sun_moon.js
+++ js/sun_moon.js
@@ -1,37 +1,48 @@
 function switchNightMode() {
-    document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="Cuteen_DarkSky"><div class="Cuteen_DarkPlanet"></div></div>'),
-      setTimeout(function() {
-        document.querySelector('body').classList.contains('DarkMode') ? (document.querySelector('body').classList.remove('DarkMode'), localStorage.setItem('isDark', '0'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')) : (document.querySelector('body').classList.add('DarkMode'), localStorage.setItem('isDark', '1'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')),
-          setTimeout(function() {
-            document.getElementsByClassName('Cuteen_DarkSky')[0].style.transition = 'opacity 3s';
-            document.getElementsByClassName('Cuteen_DarkSky')[0].style.opacity = '0';
-            setTimeout(function() {
-              document.getElementsByClassName('Cuteen_DarkSky')[0].remove();
-            }, 1e3);
-          }, 2e3)
+  document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="Cuteen_DarkSky"><div class="Cuteen_DarkPlanet"><div id="sun"></div><div id="moon"></div></div></div>'),
+      setTimeout(function () {
+          document.querySelector('body').classList.contains('DarkMode') ? (document.querySelector('body').classList.remove('DarkMode'), localStorage.setItem('isDark', '0'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')) : (document.querySelector('body').classList.add('DarkMode'), localStorage.setItem('isDark', '1'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')),
+              setTimeout(function () {
+                  document.getElementsByClassName('Cuteen_DarkSky')[0].style.transition = 'opacity 3s';
+                  document.getElementsByClassName('Cuteen_DarkSky')[0].style.opacity = '0';
+                  setTimeout(function () {
+                      document.getElementsByClassName('Cuteen_DarkSky')[0].remove();
+                  }, 1e3);
+              }, 2e3)
       })
-    const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
-    if (nowMode === 'light') {
+  const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
+  if (nowMode === 'light') {
+      document.getElementById("sun").style.opacity = "1";
+      document.getElementById("moon").style.opacity = "0";
+      setTimeout(function () {
+          document.getElementById("sun").style.opacity = "0";
+          document.getElementById("moon").style.opacity = "1";
+      }, 1000);
+
       activateDarkMode()
       saveToLocal.set('theme', 'dark', 2)
-      GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
       document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')
-    } else {
+  } else {
+      document.getElementById("sun").style.opacity = "0";
+      document.getElementById("moon").style.opacity = "1";
+      setTimeout(function () {
+          document.getElementById("sun").style.opacity = "1";
+          document.getElementById("moon").style.opacity = "0";
+      }, 1000);
       activateLightMode()
       saveToLocal.set('theme', 'light', 2)
       document.querySelector('body').classList.add('DarkMode'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')
-    }
-    // handle some cases
-    typeof utterancesTheme === 'function' && utterancesTheme()
-    typeof FB === 'object' && window.loadFBComment()
-    window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
   }
+  typeof utterancesTheme === 'function' && utterancesTheme()
+  typeof FB === 'object' && window.loadFBComment()
+  window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
+}
+// 太阳结束
 // 存数据
 // name：命名 data：数据
 function saveData(name, data) {
     localStorage.setItem(name, JSON.stringify({ 'time': Date.now(), 'data': data }))
 }
-
 // 取数据
 // name：命名 time：过期时长,单位分钟,如传入30,即加载数据时如果超出30分钟返回0,否则返回数据
 function loadData(name, time) {
@@ -43,7 +54,6 @@ function loadData(name, time) {
     }
     return 0;
 }
-
 // 上面两个函数如果你有其他需要存取数据的功能，也可以直接使用
 
 // 读取背景
@@ -52,7 +62,6 @@ try {
     if (data) changeBg(data, 1)
     else localStorage.removeItem('blogbg');
 } catch (error) { localStorage.removeItem('blogbg'); }
-
 // 切换背景函数
 // 此处的flag是为了每次读取时都重新存储一次,导致过期时间不稳定
 // 如果flag为0则存储,即设置背景. 为1则不存储,即每次加载自动读取背景.
