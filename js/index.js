37fc31952de0f3702c206bf380f0d39e76f83e05
怕冷爱上雪 <1400000222@qq.com>
2023-03-15 20:26:35 +0800

    gx

--- js/index.js
+++ js/index.js
@@ -205,3 +205,28 @@ if (window.localStorage.getItem("fpson") == undefined || window.localStorage.get
   } else {
 	document.getElementById("mydivheader").style = "display:none!important"
 }  }
+// 表
+var clock = new Vue({
+  el: '#clock',
+  data: {
+      time: '',
+      date: ''
+  }
+});
+
+var week = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+var timerID = setInterval(updateTime, 1000);
+updateTime();
+function updateTime() {
+  var cd = new Date();
+  clock.time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+  clock.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+};
+
+function zeroPadding(num, digit) {
+  var zero = '';
+  for(var i = 0; i < digit; i++) {
+      zero += '0';
+  }
+  return (zero + num).slice(-digit);
+}
